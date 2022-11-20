5f33b6cfdab0616e246f9b964995cf2507fb5774
aaron hernandez morales <ahdzmore@gmail.com>
2022-11-20 11:36:05 -0600

    actualizacion de perfil y modificaciones

--- admin/new_departamento.php
+++ admin/new_departamento.php
@@ -10,7 +10,7 @@
            padding-left: 272px;
            padding-right: 255px;
 
-       "><span class="glyphicon glyphicon-export"></span>crear mas departamento</a>
+       "><span class="glyphicon glyphicon-export"></span>crear mas Usuarios</a>
     </div><br><br><br>
 
     </h3>
@@ -20,27 +20,31 @@
 ?>
 <form id="manage_ticket" class="needs-validation"   action="./proceso_departamento/proceso_guardar.php" enctype="multipart/form-data" method="post">
   <div class="form-row">
-    <div class="col-md-4 mb-3">
-      <label id="form_departamento" for="validationTooltip01">Departamento</label>
-      <input type="text" class="form-control" id="validationTooltip01" placeholder="Departamento" name="sede" value=""  >
+    <div class="col-md-4 mb-2">
+      <label id="form_sede"  for="validationCustom02"> Departamento  </label>
+        <input type="text"   class="form-control" id="validationCustom02" placeholder="Departamento" name="sede" value=""  >
     </div>
-    <div class="col-md-3 mb-2">
-      <label id="form_telefono"  for="validationCustom02">  Telefono  </label>
-        <input type="number"   class="form-control" id="validationCustom02" placeholder="Telefono" name="telefono" value=""  >
+    <div class="col-md-4 mb-2">
+      <label id="form_telefono"  for="validationCustom02"> Telefono  </label>
+        <input type="text"   class="form-control" id="validationCustom02" placeholder="Telefono" name="telefono" value=""  >
     </div>
-    <div class="col-md-3 mb-2">
-      <label id="form_ext"  for="validationCustom02">  Extencion  </label>
-        <input type="number"   class="form-control" id="validationCustom02" placeholder="Extencion" name="ext" value=""  >
+    <div class="col-md-4 mb-2">
+      <label id="form_extencion"  for="validationCustom02">  Extencion  </label>
+        <input type="text"   class="form-control" id="validationCustom02" placeholder="ext" name="ext" value=""  >
+
     </div>
-    <div class="col-md-3 mb-2">
+    <div class="col-md-4 mb-2">
       <label id="form_direccion"  for="validationCustom02">  Direccion  </label>
         <input type="text"   class="form-control" id="validationCustom02" placeholder="Direccion" name="direccion" value=""  >
+
     </div>
-    <div class="col-md-3 mb-2">
+    <div class="col-md-4 mb-2">
       <label id="form_fecha"  for="validationCustom02">  Fecha de creacion </label>
         <?php $fechaActual = date('d/m/y H:i:s'); ?>
         <input type="text" readonly="readonly"   class="form-control" id="validationCustom02" placeholder="" name="fecha_crea" value="<?php echo $fechaActual ?>" >
     </div>
+
+
   </div> <!--Cierre-->
 <br><br><br>
   <button class="btn btn-primary" type="submit">Crear departamento</button>
